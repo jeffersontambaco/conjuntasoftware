d985db2fc949d2f93ef8bba2c4b83d81396d32d6
jeffe <jeffe@10.9.4.19>
2017-07-12 19:59:21 -0500

    Creacion de las clases

---
+++ src/main/java/ec/edu/espe/edu/conjunta/conjunta/Operacion.java
@@ -0,0 +1,15 @@
+/*
+@(#)Operacion.java     1.0 12/07/2017
+*
+*Copyright 2017 Conjunta All rights reserved.
+*Conjunta Propietary. 
+*/
+/**
+ *Clase Operacion generada todas las operaciones aritmeticas 
+ */
+package ec.edu.espe.edu.conjunta.conjunta;
+
+
+public class Operacion {
+    
+}
