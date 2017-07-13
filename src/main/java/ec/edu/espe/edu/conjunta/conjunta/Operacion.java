861e0b29c1471e7850b691a043f10317e84b65ff
JhonathanALM <jalechon@espe.edu.ec>
2017-07-12 20:06:51 -0500

    Creacion de los metodos multiplicacion y division

--- src/main/java/ec/edu/espe/edu/conjunta/conjunta/Operacion.java
+++ src/main/java/ec/edu/espe/edu/conjunta/conjunta/Operacion.java
@@ -11,5 +11,15 @@ package ec.edu.espe.edu.conjunta.conjunta;
 
 
 public class Operacion {
-    
+     public int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            return 0;
+        }
+    }
 }
