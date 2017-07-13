bf1dca01571b98d2cc281c891589e51f821bf09f
AyrtonWladimir <AyrtonWladimir@came>
2017-07-12 20:01:51 -0500

    Realizacion de las operaciones suma y resta

--- src/main/java/ec/edu/espe/edu/conjunta/conjunta/Operacion.java
+++ src/main/java/ec/edu/espe/edu/conjunta/conjunta/Operacion.java
@@ -3,13 +3,24 @@
 *
 *Copyright 2017 Conjunta All rights reserved.
 *Conjunta Propietary. 
-*/
+ */
 /**
- *Clase Operacion generada todas las operaciones aritmeticas 
+ *Clase Operacion generada todas las operaciones aritmeticas
  */
 package ec.edu.espe.edu.conjunta.conjunta;
 
-
 public class Operacion {
-    
+
+    public void suma(final int num1, final int num2) {
+        int resultado = num1 + num2;
+        System.out.println("la suma es: " + resultado);
+
+    }
+
+    public void resta(final int num1, final int num2) {
+        int resultado = num1 + num2;
+        System.out.println("la resta es: " + resultado);
+
+    }
+
 }
